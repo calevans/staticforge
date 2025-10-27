adaca991910ddbc565a1be54390ab656b2eadcaa
Cal Evans <cal@calevans.com>
2025-10-27 12:05:21 -0400

    fix: Clean up .gitignore and remove unnecessary entries; format AutoloadingTest.php

--- tests/Unit/AutoloadingTest.php
+++ tests/Unit/AutoloadingTest.php
@@ -10,10 +10,10 @@ class AutoloadingTest extends TestCase
     {
         // Test that our namespace is properly autoloaded
         $this->assertTrue(class_exists('PHPUnit\Framework\TestCase'));
-        
+
         // Test that we can access EICC\Utils namespace
         $this->assertTrue(class_exists('EICC\Utils\Container'));
-        
+
         // Verify our own namespace structure is ready
         $this->assertEquals('EICC\StaticForge\Tests\Unit', __NAMESPACE__);
     }
