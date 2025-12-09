98a2761f96026cf0c8d2d4f7f548664233c24175
Cal Evans <cal@calevans.com>
2025-12-09 16:42:58 -0500

    refactor: Clean up whitespace and comments in DownloadMediaCommand

--- src/Features/S3MediaOffload/Commands/DownloadMediaCommand.php
+++ src/Features/S3MediaOffload/Commands/DownloadMediaCommand.php
@@ -45,10 +45,10 @@ class DownloadMediaCommand extends Command
         // This implies we are downloading *to* content/assets/media.
         // If the S3 bucket has a structure like `assets/images/foo.jpg`, we should probably respect that or flatten it?
         // Let's assume the S3 bucket mirrors the public/assets structure.
-        
+
         // For now, let's list all files in the bucket.
         $files = $this->s3Service->listFiles();
-        
+
         if (empty($files)) {
             $output->writeln("<info>No files found in S3 bucket.</info>");
             return Command::SUCCESS;
@@ -62,10 +62,10 @@ class DownloadMediaCommand extends Command
             // Or keep structure?
             // If we are changing URLs to point to content/assets/media, it suggests a flat or specific structure.
             // Let's preserve the key structure relative to the target path.
-            
+
             $localFilePath = $targetPath . '/' . $key;
             $output->write("Downloading $key... ");
-            
+
             if ($this->s3Service->downloadFile($key, $localFilePath)) {
                 $output->writeln("<info>Done</info>");
             } else {
@@ -94,12 +94,12 @@ class DownloadMediaCommand extends Command
         // Let's assume standard S3 URLs for now or try to match based on the filename if we knew it.
         // But we don't know which files correspond to which S3 URLs easily without a map.
         // However, the user said "change ANY urls".
-        
+
         // A safer approach might be to look for URLs that match the files we just downloaded.
         // But that's expensive.
-        
+
         // Let's try to match generic S3 URLs.
-        
+
         foreach ($iterator as $file) {
             if ($file->isFile() && $file->getExtension() === 'md') {
                 $content = file_get_contents($file->getPathname());
@@ -110,10 +110,10 @@ class DownloadMediaCommand extends Command
                 // Note: $mediaDirectory is relative to content root.
                 // In the built site, it will be relative to root.
                 // If we download to content/assets/media, the public URL is /assets/media/...
-                
+
                 // We need to handle the leading slash correctly.
                 $replacement = '/' . $mediaDirectory . '/$1';
-                
+
                 $content = preg_replace($s3UrlPattern, $replacement, $content);
 
                 if ($content !== $originalContent) {
